d009727b70dc2d6f2ef0af05b6493e7aeff2c130
anamin1553 <anaminfathima@gmail.com>
2023-06-02 20:23:36 +0530

    change

--- lib/user/bookings.dart
+++ lib/user/bookings.dart
@@ -1,9 +1,24 @@
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 
-class BookingsPage extends StatelessWidget {
+class BookingsPage extends StatefulWidget {
   final String name;
 
   const BookingsPage({required this.name});
+
+  @override
+  State<BookingsPage> createState() => _BookingsPageState();
+}
+
+class _BookingsPageState extends State<BookingsPage> {
+  
+  void _showDatePicker(){
+    showDatePicker(context: context, 
+    initialDate: DateTime.now(), 
+    firstDate: DateTime(2000), 
+    lastDate: DateTime(2035));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +33,7 @@ class BookingsPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Center(
               child: Text(
-                'Calendar and Booking Details for $name',
+                'Calendar and Booking Details for ${widget.name}',
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
             ),
@@ -32,8 +47,9 @@ class BookingsPage extends StatelessWidget {
                   style: TextStyle(fontSize: 16.0),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    // Handle date selection
+                  onPressed: () {_showDatePicker();
+                    // TableCalendar(focusedDay: today, firstDay: firstDay, lastDay: lastDay)
+                    // // Handle date selection
                   },
                   child: Text('Pick Date'),
                 ),
